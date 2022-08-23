d96095b0b7db94874293e0a8f41115544290e7d5
cnx-germanolira <dolan.3@hotmail.com>
2022-08-23 12:13:39 -0300

    create project

--- home.js
+++ home.js
@@ -1,27 +1,21 @@
 import React from "react";
-import { NavigationContainer } from "@react-navigation/native";
-import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
-import { Home } from "./home";
+import { View, Text } from "react-native";
 
-export default function App() {
-  const Tab = createBottomTabNavigator();
-
+export function Home() {
   return (
-    <NavigationContainer>
-      <Tab.Navigator>
-        <Tab.Screen name="Home" component={Home} />
-      </Tab.Navigator>
-    </NavigationContainer>
+    <View>
+      <Text>Hello World</Text>
+    </View>
   );
 }
 
-// Skia without tab navigation
+// Skia with tab navigation
 
 // import { StatusBar } from "expo-status-bar";
 // import React from "react";
 // import { Canvas, Text, useFont, Fill } from "@shopify/react-native-skia";
 
-// export default function App() {
+// export function Home() {
 //   const fontSize = 32;
 //   const font = useFont(require("./roboto.ttf"), fontSize);
 //   if (font === null) {
