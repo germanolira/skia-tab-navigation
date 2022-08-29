861d870421fe9b0fbdc39001a52d39027cdf407b
cnx-germanolira <dolan.3@hotmail.com>
2022-08-29 19:47:30 -0300

    update example

--- home.js
+++ home.js
@@ -1,37 +1,37 @@
 // home.js
 
-import { StatusBar } from "expo-status-bar";
-import React from "react";
-import { View, Text } from "react-native";
+// import { StatusBar } from "expo-status-bar";
+// import React from "react";
+// import { View, Text } from "react-native";
 
-export function Home() {
-  return (
-    <View>
-      <Text>Hello World</Text>
-    </View>
-  );
-}
+// export function Home() {
+//   return (
+//     <View>
+//       <Text>Hello World</Text>
+//     </View>
+//   );
+// }
 
 // Skia with tab navigation
 
-// import { StatusBar } from "expo-status-bar";
-// import React from "react";
-// import { Canvas, Text, useFont, Fill } from "@shopify/react-native-skia";
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { Canvas, Text, useFont, Fill } from "@shopify/react-native-skia";
 
-// export function Home() {
-//   const fontSize = 32;
-//   const font = useFont(require("./roboto.ttf"), fontSize);
-//   if (font === null) {
-//     return null;
-//   }
+export function Home() {
+  const fontSize = 32;
+  const font = useFont(require("./roboto.ttf"), fontSize);
+  if (font === null) {
+    return null;
+  }
 
-//   return (
-//     <>
-//       <Canvas style={{ flex: 1 }}>
-//         <Fill color="white" />
-//         <Text x={0} y={fontSize} text="Hello World" font={font} />
-//       </Canvas>
-//       <StatusBar style="auto" />
-//     </>
-//   );
-// }
+  return (
+    <>
+      <Canvas style={{ flex: 1 }}>
+        <Fill color="white" />
+        <Text x={0} y={fontSize} text="Hello World" font={font} />
+      </Canvas>
+      <StatusBar style="auto" />
+    </>
+  );
+}
